ce9182c042a9627876b31567558a488945c60a1c
j.bolek <janbolek97@gmail.com>
2024-11-17 21:12:43 +0100

    code upgrade

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 def main ():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        length = len(words)
-    print(length)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    low = lowercase(text)
+    print(low)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def lowercase(text):
+    lowercase_text = text.lower()
+    return lowercase_text
 
 main()
